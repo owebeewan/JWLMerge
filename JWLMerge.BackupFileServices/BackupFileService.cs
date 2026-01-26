944e399a3ceb57871cbcf8b1ac952df520ffde35
Wes Williams <wmwilliams@bethel.jw.org>
2026-01-26 15:39:57 -0500

    Minor sync diffs

--- JWLMerge.BackupFileServices/BackupFileService.cs
+++ JWLMerge.BackupFileServices/BackupFileService.cs
@@ -83,7 +83,7 @@ public sealed class BackupFileService : IBackupFileService
     /// <inheritdoc />
     public void RemoveFavourites(BackupFile backup)
     {
-        ArgumentNullException.ThrowIfNull(backup);
+        ArgumentNullException.ThrowIfNull(backup, nameof(backup));
 
         backup.Database.TagMaps.RemoveAll(x => x.TagId == 1);
     }
@@ -506,13 +506,10 @@ public sealed class BackupFileService : IBackupFileService
     {
         Log.Logger.Debug("Updating manifest");
 
-        Manifest result = manifestToBaseOn.Clone();
+        var result = manifestToBaseOn.Clone();
 
-        DateTime now = DateTime.Now;
-        string simpleDateString = $"{now.Year}-{now.Month:D2}-{now.Day:D2}";
-
-        result.Name = $"merged_{simpleDateString}";
-        result.CreationDate = simpleDateString;
+        result.Name = $"JWLMerge";
+        result.CreationDate = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK", CultureInfo.InvariantCulture);
         result.UserDataBackup.DeviceName = "JWLMerge";
         result.UserDataBackup.DatabaseName = DatabaseEntryName;
 
